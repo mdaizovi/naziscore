8fb4dd788152a11335e6e2ede84fd6c46d2b6b26
Ricardo Bánffy <rbanffy@gmail.com>
2017-05-13 13:22:20 +0100

    Log the grader calls

--- src/naziscore/scoring.py
+++ src/naziscore/scoring.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import logging
+
 from inspect import isfunction
 
 
@@ -7,10 +9,11 @@ def calculated_score(profile_json, posts_json):
     """Returns the score for the recent posts JSON"""
     g = globals()
     total = 0
-    for f in [
+    for grader in [
             g[f] for f in g
             if isfunction(g[f]) and f.startswith('points_from')]:
-        total += f(profile_json, posts_json)
+        logging.info('Calling ' + grader.__repr__())
+        total += grader(profile_json, posts_json)
     return total
 
 
