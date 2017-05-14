804337a7b220f08b83c861861c977f689142e125
Ricardo Bánffy <rbanffy@gmail.com>
2017-05-14 01:40:11 +0100

    Fleshing out grader functions

--- src/naziscore/scoring.py
+++ src/naziscore/scoring.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import json
 import logging
 
 from inspect import isfunction
@@ -9,17 +10,31 @@ def calculated_score(profile_json, posts_json):
     """Returns the score for the recent posts JSON"""
     g = globals()
     total = 0
+    profile = json.loads(profile_json)
+    timeline = json.loads(posts_json)
     for grader in [
             g[f] for f in g
             if isfunction(g[f]) and f.startswith('points_from')]:
         logging.info('Calling ' + grader.__repr__())
-        total += grader(profile_json, posts_json)
+        total += grader(profile, timeline)
     return total
 
 
-def points_from_gabai(profile_json, timeline_json):
-    return 1
+def points_from_gabai(profile, timeline):
+    "Returns 1 if 'gab.ai' is in the profile name or description."
+    result = (1 if 'gab.ai' in profile['description']
+              or 'gab.ai' in profile['name']
+              else 0)
+    if result > 0:
+        logging.info(profile['screen_name'] + 'tested positive for gab.ai')
+    return result
 
 
-def points_from_pepes(profile_json, timeline_json):
-    return 1
+def points_from_pepes(profile, timeline):
+    "Returns the number of different racist symbols in the name."
+    result = 0
+    for pepe in u"🐸🥛👌":  # Pepe, milk and OK
+        result += 1 if pepe in profile['name'] else 0
+    if result > 0:
+        logging.info(profile['screen_name'] + 'tested positive for pepe')
+    return result
