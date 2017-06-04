53823c9c7a7d2580649a23265677259af6f6156e
Ricardo Bánffy <rbanffy@gmail.com>
2017-06-04 13:31:06 +0100

    Support queries by numeric id

--- src/naziscore/main.py
+++ src/naziscore/main.py
@@ -5,7 +5,10 @@ import webapp2
 
 from google.appengine.ext import ndb
 
-from naziscore.handlers import ScoreHandler
+from naziscore.handlers import (
+    ScoreByIdHandler,
+    ScoreByNameHandler,
+)
 
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
@@ -15,8 +18,11 @@ app = webapp2.WSGIApplication(
         # calculated yet. If the score is not calculates, schedule a crawl over
         # the profile.
         webapp2.Route(
-            '/v1/<profile_id:.{1,15}>/score.json',
-            ScoreHandler, name='score_handler_v1'),
+            '/v1/screen_name/<screen_name:.{1,15}>/score.json',
+            ScoreByNameHandler, name='score_handler_by_screen_name_v1'),
+        webapp2.Route(
+            '/v1/twitter_id/<twitter_id:\d*>/score.json',
+            ScoreByIdHandler, name='score_handler_by_idv1'),
     ],
     debug=DEBUG)
 
--- src/naziscore/models.py
+++ src/naziscore/models.py
@@ -5,7 +5,8 @@ from google.appengine.ext import ndb
 
 class Score(ndb.Model):
     """Our sample class"""
-    profile_id = ndb.StringProperty()
+    screen_name = ndb.StringProperty()
+    twitter_id = ndb.IntegerProperty()
     last_updated = ndb.DateTimeProperty(auto_now=True)
     score = ndb.FloatProperty()
 
