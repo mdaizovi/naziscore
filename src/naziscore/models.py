947bd852ec9d8707bc999eec2ef775a9c2098590
Ricardo Bánffy <rbanffy@gmail.com>
2017-06-10 13:51:20 +0100

    Add a property to keep various components of a score

--- src/naziscore/models.py
+++ src/naziscore/models.py
@@ -6,9 +6,14 @@ from google.appengine.ext import ndb
 class Score(ndb.Model):
     """Our sample class"""
     screen_name = ndb.StringProperty()
+    screen_name_lower = ndb.ComputedProperty(
+        lambda self: self.screen_name.lower())
     twitter_id = ndb.IntegerProperty()
     last_updated = ndb.DateTimeProperty(auto_now=True)
     score = ndb.FloatProperty()
+    grade = ndb.ComputedProperty(
+        lambda self: sum([self.grades[k] for k in self.grades]))
+    grades = ndb.JsonProperty()
 
     profile_text = ndb.TextProperty()
     timeline_text = ndb.TextProperty()
