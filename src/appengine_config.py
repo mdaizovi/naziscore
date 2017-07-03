10c371e0f86df8f9b399433934254fd558c0786b
Ricardo Banffy <rbanffy@gmail.com>
2017-07-03 18:32:00 +0100

    Make stats collection only on local dev environment

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
 
+import os
+
+DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+
+
 def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
+    if DEBUG:
+        from google.appengine.ext.appstats import recording
+        app = recording.appstats_wsgi_middleware(app)
     return app
