4506744a02236cea44298bd8b784fe8011106d10
Ricardo Banffy <rbanffy@gmail.com>
2017-08-03 14:38:32 +0100

    Remove logging from expansion, add TODO note

---
+++ src/naziscore/utils.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import logging
+
+from google.appengine.api import urlfetch
+
+
+def expanded_url(url):
+    "Expands the URL using the location header protocol. Returns the URL."
+    # TODO: Memcache URLs - popular ones will tend to repeat.
+    while True:
+        eu = urlfetch.Fetch(
+            url, follow_redirects=False).headers.get('location', url)
+        if url == eu:
+            return eu
+        else:
+            url = eu
