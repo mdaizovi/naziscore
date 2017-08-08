d55a8e807a1056425a86c046f8868d9aee0024bf
Ricardo Bánffy <rbanffy@gmail.com>
2017-08-08 19:35:43 +0100

    Guard against DNS failures

--- src/naziscore/utils.py
+++ src/naziscore/utils.py
@@ -3,15 +3,18 @@
 import logging
 
 from google.appengine.api import urlfetch
-
+from google.appengine.api.urlfetch_errors import DNSLookupFailedError
 
 def expanded_url(url):
     "Expands the URL using the location header protocol. Returns the URL."
     # TODO: Memcache URLs - popular ones will tend to repeat.
     while True:
-        eu = urlfetch.Fetch(
-            url, follow_redirects=False).headers.get('location', url)
-        if url == eu:
-            return eu
-        else:
-            url = eu
+        try:
+            eu = urlfetch.Fetch(
+                url, follow_redirects=False).headers.get('location', url)
+            if url == eu:
+                return eu
+            else:
+                url = eu
+        except DNSLookupFailedError:
+            return url  # Return the last good one.
