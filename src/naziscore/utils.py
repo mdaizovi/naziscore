71b5354a288651b04867c9993863592e48f4e81f
Ricardo Bánffy <rbanffy@gmail.com>
2017-08-20 15:55:27 +0100

    Use memcache for URL expansion

--- src/naziscore/utils.py
+++ src/naziscore/utils.py
@@ -1,11 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import logging
+import urllib
 import urlparse
 
 from ssl import CertificateError
 
-from google.appengine.api import urlfetch
+from google.appengine.api import (
+    memcache,
+    urlfetch
+)
 from google.appengine.api.urlfetch_errors import (
     DeadlineExceededError,
     DownloadError,
@@ -19,6 +23,12 @@ from google.appengine.api.urlfetch_errors import (
 def expanded_url(url):
     "Expands the URL using the location header protocol. Returns the URL."
     # TODO: Memcache URLs - popular ones will tend to repeat.
+    key = urllib.quote_plus(urlparse.urlparse(url).geturl())
+    expanded = memcache.get(key)
+    if expanded is not None:
+        logging.info('URL expansion cache hit.')
+        return expanded
+    logging.info('URL expansion cache miss.')
     while True:
         try:
             eu = urlfetch.Fetch(
@@ -26,8 +36,11 @@ def expanded_url(url):
             purl = urlparse.urlparse(eu)
             if purl.scheme == '':  # It's relative (hopefully root-relative)
                 purl = urlparse.urlparse(url)
-                return purl.scheme + '://' + purl.netloc + eu
+                expanded = purl.scheme + '://' + purl.netloc + eu
+                memcache.set(key, expanded)
+                return expanded
             if url == eu:
+                memcache.set(key, eu)
                 return eu
             else:
                 url = eu
@@ -40,6 +53,7 @@ def expanded_url(url):
                 DNSLookupFailedError,
                 ResponseTooLargeError,
                 SSLCertificateError) as e:
-            # This is as far as we'll go expanding this URL
+            # This is as far as we'll go expanding this URL, but since errors
+            # can be transient, we won't cache this.
             logging.error('fetching {} resulted in {}'.format(url, e))
             return url  # Return the last good one.
